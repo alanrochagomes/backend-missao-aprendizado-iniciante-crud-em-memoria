9430516ef9ae5bf092099ea26bff57fd48579a2b
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-13 20:39:46 +0000

    Implementação do Create `[POST] /personagem`

--- index.js
+++ index.js
@@ -25,4 +25,22 @@ app.get('/personagem/:id', function (req, res) {
     res.send(item)
 })
 
+// Sinalizo para o Express que estamos usando JSON no Body
+app.use(express.json())
+
+// Endpoint Create [POST] /personagem
+app.post('/personagem', function (req, res) {
+  // Acessamos o Body da Requisição
+  const body = req.body
+  
+// Acessamos a propriedade `nome` do body
+  const novoItem = body.nome
+
+  // Adicionamos na lista
+  lista.push(novoItem)
+
+  // Exibimos uma mensagem de sucesso
+  res.send('Item adicionado com sucesso: ' + novoItem)
+})
+
 app.listen(3000)
