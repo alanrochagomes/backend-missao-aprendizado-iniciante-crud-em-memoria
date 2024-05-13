bcd477ff87cfaad1a747eedab10305e2b18f6713
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-13 21:05:11 +0000

    Endpoint Delete `[DELETE] /personagem/:id` implementado

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const lista = ['Java', 'Kotlin', 'Android']
 
 // Endpoint Read All [GET] /personagem
 app.get('/personagem', function (req, res) {
-    res.send(lista)
+    res.send(lista.filter(Boolean))
 })
 
 // Endpoint Read By Id [GET] /personagem/:id
@@ -59,6 +59,18 @@ app.put('/personagem/:id', function (req, res) {
 
   // Enviamos uma mensagem de sucesso
   res.send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
+})
+
+// Endpoint Delete [DELETE] /personagem/:id
+app.delete('/personagem/:id', function (req, res) {
+  // Acessamos o parâmetro de rota
+  const id = req.params.id
+
+  // Remover o item da lista usando o ID - 1
+  delete lista[id - 1]
+
+  // Enviamos uma mensagem de sucesso 
+  res.send('Item removido com sucesso: ' + id)
 })
 
 app.listen(3000)
